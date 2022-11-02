dd5192147b2ef9fbb5eb40e5e912b4ffe92d9538
thebiswa <biswarup.murmu.ee2019@kgec.ac.in>
2022-11-02 22:19:03 +0530

    Fixed no scroll problem when screen is resized
    1. Edited ektara.js to solve the problem Problem(1)
    2. Added functions in body tag html

--- javascript/ektara.js
+++ javascript/ektara.js
@@ -1,3 +1,73 @@
+/* Every section starts with a section name like A B ..... Sections are devided into sub-sections line A1 A2 */
+/* Problems are denoted like Problem(1) Problem(2) ............... */
+
+
+
+
+
+/* SECTION A */
+
+/* These two functions will know the screen size the user is using and update the userdevice attribute in body tag. */
+
+/* SECTION A1 */
+/* This function will be called when the page relodes or loads for the first time */
+function knowscreenSizeonload() {
+  var w = window.outerWidth;
+  let sizeoftheScreen = document.getElementById('body');
+  device = sizeoftheScreen.getAttribute('userdevice');
+  if(w>650) {
+      sizeoftheScreen.setAttribute("userdevice",'desktop');
+      console.log('The user is using a desktop( wider screen >650px)');
+  }
+  else {
+      sizeoftheScreen.setAttribute("userdevice",'mobile');
+      console.log('The user is is using mobile(smaller screen width <=650px)');
+  }
+  //console.log(device);
+}
+
+/* SECTION A2 */
+/* This function will be called when the user changes the screen orientation or resize the screen */
+
+function screenSizeonresizewindow() {
+  var w = window.outerWidth;
+  let sizeoftheScreen = document.getElementById('body');
+  device = sizeoftheScreen.getAttribute('userdevice');
+  if(w>650) {
+      sizeoftheScreen.setAttribute("userdevice",'desktop');
+      console.log('The user has made the screen wider(screen width >650px)');
+
+    /* This section is the solution of the problem Problem(1)*/
+    const navMenu = document.querySelector(".navmenu");
+    const visibility = navMenu.getAttribute('data-visible');
+      if(visibility ==="true") {
+        var element = document.getElementById("body");
+        element.classList.toggle("noscroll");
+      }
+    /* This section is the solution of the problem Problem(1) End*/
+  }
+  else {
+      sizeoftheScreen.setAttribute("userdevice",'mobile');
+      console.log('The user has made the screen narrower(screen width <=650px)');
+
+    /* This section is the solution of the problem Problem(1)*/
+    const navMenu = document.querySelector(".navmenu");
+    const visibility = navMenu.getAttribute('data-visible');
+      if(visibility ==="true") {
+        var element = document.getElementById("body");
+        element.classList.toggle("noscroll");
+      }
+    /* This section is the solution of the problem Problem(1) End*/
+
+  }
+  //console.log(device);
+}
+
+/* SECTION A ends*/
+
+
+/* SECTION B */
+
 /* Sticky navigation bar */
 var prevScrollpos = window.pageYOffset;
 window.onscroll = function() {
@@ -10,14 +80,19 @@ var currentScrollPos = window.pageYOffset;
   prevScrollpos = currentScrollPos;
 }
 
+/* SECTION B ends*/
+
+/* SECTION C */
 
   /* Animated hamburger menu*/
 function myFunction(x) {
     x.classList.toggle("change");
   }
 
+/* SECTION C ends */
 
 
+/* SECTION D */
 
 /* In mobile view - Open and close the sidebar using hamburger menu option */
 const navMenu = document.querySelector(".navmenu");
@@ -25,10 +100,19 @@ const toggleButton = document.querySelector(".togglebutton")
 
 toggleButton.addEventListener("click",() => {
   const visibility = navMenu.getAttribute('data-visible');
+  console.log(device);
 
-  /* Block scroll when sidebar is open */
+  /* Block scroll when sidebar is open ( This section has a problem: Problem(1).
+  It alone creates a problem but the problem is solved above SECTION A2) */
+
   var element = document.getElementById("body");
   element.classList.toggle("noscroll");
+  /*  Problem(1) ---->
+  when sidebar is opened and the mobile phonne is set in auto rotate mode
+  and the orientation of the phone changes as the user tilts his/her phone, the screen becomes horizontally bigger and acts as
+  a desktop screen. and the sidebar is hidden and navbar is visible. but as the bosy is set to no scroll, user cannot
+  scroll the page anymore. To solve this problem using the screen resize function*/
+
   /* Block scroll when sidebar is open */
   
   if(visibility ==="false") {
@@ -41,6 +125,11 @@ toggleButton.addEventListener("click",() => {
   console.log(visibility)
 } );
 /* In mobile view - Open and close the sidebar using hamburger menu option */
+
+/* SECTION D ends*/
+
+
+
 
 
 
